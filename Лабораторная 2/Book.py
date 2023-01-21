36b38053b3e00a89a08dc76b3aa2d9e7d0aa3874
PalMorthy <115100210+PalMorthy@users.noreply.github.com>
2023-01-21 21:36:39 +0300

    feat(Book, Lib): added new files

---
+++ Лабораторная 2/Book.py
@@ -0,0 +1,46 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        self.id = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        """
+        Returns the format string Book "book title"
+        """
+        true_format = f'Книга "{self.name}"'
+        return true_format
+
+    def __repr__(self) -> str:
+        """
+        Returns a valid python string
+        from which exactly the same instance can be initialized
+        """
+        valid_string = f"Book(id_={self.id}, name='{self.name}', pages={self.pages})"
+        return valid_string
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
