54413dbdf540fb515fe5ba1d56aa62c7f8ddd86d
PalMorthy <115100210+PalMorthy@users.noreply.github.com>
2023-02-12 19:54:41 +0300

    fixed

--- Books.py
+++ Books.py
@@ -10,21 +10,39 @@ class Book:
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r})"
 
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
+
+    @property
+    def author(self) -> str:
+        return self.__author
+
+    @author.setter
+    def author(self, author: str):
+        self.__author = author
+
 
 class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
         super().__init__(name, author)
-        """
-        TEST UNIT:
-        ~~~~~~~~~~
-        """
+        self.__pages = pages
+
+    @property
+    def pages(self) -> int:
+        return self.__pages
+
+    @pages.setter
+    def pages(self, pages: int):
+        if 0 > pages:
+            raise ValueError('pages < 0')
         if not isinstance(pages, int):
-            raise TypeError("Pages must be int!!!")
-        if pages < 0:
-            raise ValueError("Amount of Pages must be positive")
-        self.__name = name
-        self.__author = author
-        self.pages = pages
+            raise TypeError("pages isn't int")
+        self.__pages = pages
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r}, pages={self.pages})"
@@ -33,13 +51,21 @@ class PaperBook(Book):
 class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
         super().__init__(name, author)
-        if not isinstance(duration, float):
-            raise TypeError("Duration must be int!!!")
-        if duration < 0:
-            raise ValueError("Duration must be positive!!!")
-        self.__name = name
-        self.__author = author
-        self.duration = duration
+        self.__duration = duration
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.__name!r}, author={self.__author!r}, duration={self.duration})"
+
+    @property
+    def duration(self) -> float:
+        return self.__duration
+
+    @duration.setter
+    def duration(self, duration: float):
+        if duration < 0:
+            raise ValueError("duration < 0")
+        if not isinstance(duration, float):
+            raise TypeError("duration isn't float")
+        self.__duration = duration
+
+
